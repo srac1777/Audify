33c2cf265a183fe1763bd2bf1c61c6fbeba95d2e
Shashank Racherla <shashank.racherla@gmail.com>
2018-06-06 17:18:55 -0700

    Assets issue v2

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as UtilTest from './utils/session_api_util';
 import configureStore from './store/store';
-import Root from './components/root';
+import Root from './components/Root';
 
 
 document.addEventListener('DOMContentLoaded', () => {
